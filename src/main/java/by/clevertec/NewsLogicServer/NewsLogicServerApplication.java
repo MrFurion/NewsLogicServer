83d4bcd6d28eabb10b78aae919db2a92b9c3cce0
Trubetski <trubetski.a@gmail.com>
2024-12-15 12:56:46 +0300

    Initial commit

---
+++ src/main/java/by/clevertec/NewsLogicServer/NewsLogicServerApplication.java
@@ -0,0 +1,13 @@
+package by.clevertec.NewsLogicServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsLogicServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsLogicServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/clevertec/NewsLogicServer/NewsLogicServerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.clevertec.NewsLogicServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsLogicServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
